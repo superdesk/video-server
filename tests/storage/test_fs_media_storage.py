3d094403cedf55f62b850a2c440aa35e65da18c9
Thanh Nguyen <thanh.nguyentan@idsolutions.com.vn>
2019-04-24 15:42:07 +0700

    add pytest for storage

--- tests/storage/test_fs_media_storage.py
+++ tests/storage/test_fs_media_storage.py
@@ -1,20 +1,26 @@
 import os
-
 import pytest
+import shutil
 
 from media import get_media_collection
 from media.storage.fs_media_storage import FileSystemMediaStorage
 
 storage = FileSystemMediaStorage()
 
+
 def test_put_fs_media_storage(client, test_path):
     with open(f'{test_path}/storage/fixtures/sample.mp4', 'rb') as f:
         filestream = f.read()
+    from media.storage import fs_media_storage
+    fs_media_storage.PATH_FS = os.path.dirname(__file__) + '/fs'
     filename = 'test_fs_media_storage'
     doc = storage.put(filestream, filename, None, 'mp4')
-    
+    filename = doc.get('filename')
+    dir_file = doc.get('folder')
+    file_path = "%s/%s/%s" % (fs_media_storage.PATH_FS, dir_file, filename)
     assert doc['filename'] == filename
     assert doc['metadata'] == None
     assert doc['mime_type'] == 'mp4'
-
+    assert os.path.exists(file_path) == True
     get_media_collection().delete_one({'_id': doc['_id']})
+    shutil.rmtree(fs_media_storage.PATH_FS)
