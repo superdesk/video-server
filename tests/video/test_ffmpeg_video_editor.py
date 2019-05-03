683198d429dba0f2a9c0a2e031a0721e8bedbf44
olegpshenichniy <olegpshenichniy@gmail.com>
2019-05-03 17:09:32 +0200

    Setup and teardown test media folder and test db before and after every test with test_app fixture.

--- tests/video/test_ffmpeg_video_editor.py
+++ tests/video/test_ffmpeg_video_editor.py
@@ -1,10 +1,10 @@
-from lib.video_editor.ffmpeg import FFMPEGVideoEditor
 import sys
+from lib.video_editor.ffmpeg import FFMPEGVideoEditor
 
 editor = FFMPEGVideoEditor()
 
 
-def test_ffmpeg_video_editor_cut_video(client, filestream):
+def test_ffmpeg_video_editor_cut_video(filestream):
     content, metadata = editor.edit_video(
         filestream, 'test_ffmpeg_video_editor_cut_video.mp4', None,
         video_cut={'start': 2, 'end': 10}
@@ -17,7 +17,7 @@ def test_ffmpeg_video_editor_cut_video(client, filestream):
     assert metadata['bit_rate'] == '332180'
 
 
-def test_ffmpeg_video_editor_crop_video(client, filestream):
+def test_ffmpeg_video_editor_crop_video(filestream):
     content, metadata = editor.edit_video(
         filestream, 'test_ffmpeg_video_editor_crop_video.mp4', None,
         video_crop={'width': 500, 'height': 400, 'x': 10, 'y': 10}
@@ -30,7 +30,7 @@ def test_ffmpeg_video_editor_crop_video(client, filestream):
         assert metadata['size'] == '2353910'
 
 
-def test_ffmpeg_video_editor_rotate_video(client, filestream):
+def test_ffmpeg_video_editor_rotate_video(filestream):
     content, metadata = editor.edit_video(
         filestream, 'test_ffmpeg_video_editor_rotate_video.mp4', None,
         video_rotate={'degree': 90}
