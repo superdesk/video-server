93a3c1699d52be42fc10d2b2230a0576b42eafbc
Loi Tran <loi.tran@idsolutions.com.vn>
2019-05-07 16:40:29 +0700

    Fix ffmpeg test case

--- tests/video/test_ffmpeg_video_editor.py
+++ tests/video/test_ffmpeg_video_editor.py
@@ -14,8 +14,8 @@ def test_ffmpeg_video_editor_cut_video(filestream):
         video_cut={'start': 2, 'end': 10}
     )
 
-    assert metadata['width'] == '640'
-    assert metadata['height'] == '480'
+    assert metadata['width'] == 640
+    assert metadata['height'] == 480
     assert metadata['duration'] == '8.027031'
     assert metadata['size'] == '666985'
     assert metadata['bit_rate'] == '332180'
@@ -27,8 +27,8 @@ def test_ffmpeg_video_editor_crop_video(filestream):
         video_crop={'width': 500, 'height': 400, 'x': 10, 'y': 10}
     )
 
-    assert metadata['width'] == '500'
-    assert metadata['height'] == '400'
+    assert metadata['width'] == 500
+    assert metadata['height'] == 400
     if sys.platform == 'linux':
         assert metadata['bit_rate'] == '553486'
         assert metadata['size'] == '2353910'
@@ -40,22 +40,23 @@ def test_ffmpeg_video_editor_rotate_video(filestream):
         video_rotate={'degree': 90}
     )
 
-    assert metadata['width'] == '480'
-    assert metadata['height'] == '640'
+    assert metadata['width'] == 480
+    assert metadata['height'] == 640
 
 
 def test_ffmpeg_video_editor_generate_thumbnails(client, filestream):
     metadata = editor.get_meta(filestream)
+    fs_path = app.config['FS_MEDIA_STORAGE_PATH']
     filename = 'test_ffmpeg_video_editor_generate_thumbnails'
-    for index, thumb in enumerate(
+    for index, (thumbnail, thumbnail_meta) in enumerate(
         editor.capture_list_timeline_thumbnails(
             filestream,
             filename,
             metadata,
-            app.config.get('AMOUNT_FRAMES', 40))
-    ):
-        app.fs.put(thumb[0], f'test_generate_thumbnails/filename_{index}.png')
-    list_files = os.listdir(app.config['FS_MEDIA_STORAGE_PATH'] + f'/test_generate_thumbnails')
+            app.config.get('AMOUNT_FRAMES', 40))):
+        app.fs.put(thumbnail, f'{fs_path}/test_generate_thumbnails/filename_{index}.png')
+
+    list_files = os.listdir(f'{fs_path}/test_generate_thumbnails')
     list_files = [fi for fi in list_files if fi.startswith(f'filename_')]
 
     assert len(list_files) == 41
