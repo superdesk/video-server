fbeb9af12b249e8faabdf8f3ab57b3e719a781a7
Thanh Nguyen <thanh.nguyentan@idsolutions.com.vn>
2019-11-25 17:52:43 +0700

    fix test case file-storage and update some stuff

--- src/videoserver/lib/storage/file_system_storage.py
+++ src/videoserver/lib/storage/file_system_storage.py
@@ -67,7 +67,8 @@ class FileSystemStorage(MediaStorageInterface):
 
         return media_file
 
-    def put(self, content, filename, project_id=None, asset_type='project', storage_id=None, content_type=None, override=True):
+    def put(self, content, filename, project_id=None, asset_type='project', storage_id=None, content_type=None,
+            override=True):
         """
         Save file into a fs storage.
 
@@ -112,7 +113,7 @@ class FileSystemStorage(MediaStorageInterface):
         if os.path.exists(file_path):
             if not override:
                 raise Exception(f'File {file_path} already exists, use "replace" method instead.')
-            self.replace(content,storage_id)
+            self.replace(content, storage_id)
 
         # check if dir exists, if not create it
         file_dir = os.path.dirname(file_path)
--- src/videoserver/lib/utils.py
+++ src/videoserver/lib/utils.py
@@ -92,21 +92,12 @@ def add_urls(doc):
                     _external=True
                 )
 
-            if doc['thumbnails']['preview']:
+            if doc['thumbnails']['preview'] or doc['processing']['thumbnail_preview']:
                 doc['thumbnails']['preview']['url'] = url_for(
                     'projects.get_raw_preview_thumbnail',
                     project_id=doc['_id'],
                     _external=True
                 )
-
-            if doc['processing']['thumbnail_preview']:
-                doc['thumbnails']['preview'] = {
-                    "url": url_for(
-                        'projects.get_raw_preview_thumbnail',
-                        project_id=doc['_id'],
-                        _external=True
-                    ),
-                }
 
     if type(doc) is dict:
         _handle_doc(doc)
