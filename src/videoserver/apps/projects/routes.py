7827f64a13ad74a32c119fac0db82c2903a290f5
lambiengcode <lambiengcode@gmail.com>
2023-05-09 14:05:31 +0700

    feat(s3): upload video when create

--- src/videoserver/apps/projects/routes.py
+++ src/videoserver/apps/projects/routes.py
@@ -2,6 +2,7 @@ import copy
 import logging
 import os
 import re
+import io
 from datetime import datetime
 
 import bson
@@ -15,14 +16,13 @@ from videoserver.lib.video_editor import get_video_editor
 from videoserver.lib.views import MethodView
 from videoserver.lib.utils import (
     add_urls, create_file_name, get_request_address, json_response, paginate, save_activity_log, storage2response,
-    validate_document
+    validate_document, upload_file_to_s3,
 )
 
 from . import bp
 from .tasks import edit_video, generate_preview_thumbnail, generate_timeline_thumbnails
 
 logger = logging.getLogger(__name__)
-
 
 class ListUploadProject(MethodView):
     SCHEMA_UPLOAD = {
@@ -141,18 +141,21 @@ class ListUploadProject(MethodView):
             raise BadRequest({"file": ["required field"]})
         document = validate_document(request.files, self.SCHEMA_UPLOAD)
 
-        print("START HERE")
-
         # validate codec
+        # user_file = request.files['file']
         file_stream = document['file'].stream.read()
         metadata = get_video_editor().get_meta(file_stream)
         if metadata.get('codec_name') not in app.config.get('CODEC_SUPPORT_VIDEO'):
             raise BadRequest({'file': [f"Codec: '{metadata.get('codec_name')}' is not supported."]})
 
+        file_name_lbc = create_file_name(ext=document['file'].filename.rsplit('.')[-1])
+        file_name_s3 = 'videos/' + file_name_lbc
+
         # add record to database
         project = {
+            'urlToVideoS3': os.getenv('AWS_DOMAIN') + file_name_s3,
             '_id': bson.ObjectId(),
-            'filename': create_file_name(ext=document['file'].filename.rsplit('.')[-1]),
+            'filename': file_name_lbc,
             'storage_id': None,
             'metadata': metadata,
             'create_time': datetime.utcnow(),
@@ -172,8 +175,6 @@ class ListUploadProject(MethodView):
             }
         }
 
-        print("START HERE 2")
-
         # put file stream into storage
         storage_id = app.fs.put(
             content=file_stream,
@@ -184,7 +185,10 @@ class ListUploadProject(MethodView):
         # set 'storage_id' for project
         project['storage_id'] = storage_id
 
-        print("START HERE 3")
+        upload_succeed = upload_file_to_s3(storage_id, file_name_s3, document['file'].mimetype)
+
+        if not upload_succeed:
+          raise InternalServerError("Upload file to s3 error!")
 
         try:
             # save project
